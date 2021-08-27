69df8e3b9637e943eb2e169968b278ba12a24fae
unknown <crsn1111@gmail.com>
2021-08-27 13:07:24 +0900

    year substring error

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -55,13 +55,15 @@ const TVPresenter = ({ topRated,
           imageUrl={show.poster_path}
           title={show.original_name}
           rating={show.vote_average}
-          year={show.first_air_date.substring(0, 4)}
+          year={show.first_air_date}
         />
       ))}
     </Section>
     )}
     {error && <Error text={error}/>}
-  </Container>)
+  </Container>
+)
+  
 
 TVPresenter.propTypes = {
   topRated: PropTypes.array,
